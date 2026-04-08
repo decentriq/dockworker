8909015e0d22b25fd9ad52ae3b6b4b6c9dcdc864
Isak Johansson <isak.johansson@decentriq.ch>
2026-04-08 08:49:47 +0000

    updates required for new vm
    - Fix structs to comply with the api version
    - Make the error message contain the context

--- src/errors.rs
+++ src/errors.rs
@@ -6,32 +6,33 @@ use thiserror::Error;
 
 pub type Result<T> = ::std::result::Result<T, Error>;
 
+// This has been modified to return the actual error message.
 #[derive(Error, Debug)]
 pub enum Error {
-    #[error("io error")]
+    #[error("io error: {0:?}")]
     Io(#[from] io::Error),
-    #[error("envvar error")]
+    #[error("envvar error: {0:?}")]
     Envvar(#[from] env::VarError),
-    #[error("hyper error")]
+    #[error("hyper error: {0:?}")]
     Hyper(#[from] hyper::Error),
-    #[error("json error")]
+    #[error("json error: {0:?}")]
     Json(#[from] serde_json::Error),
-    #[error("docker error")]
+    #[error("docker error: {0:?}")]
     Docker(#[from] docker::DockerError),
-    #[error("response error")]
+    #[error("response error: {0:?}")]
     Response(#[from] response::Error),
-    #[error("http error")]
+    #[error("http error: {0:?}")]
     Http(#[from] http::Error),
-    #[error("invalid uri")]
+    #[error("invalid uri: {source:?}")]
     InvalidUri {
         var: String,
         source: http::uri::InvalidUri,
     },
     #[cfg(feature = "native-tls")]
-    #[error("ssl error")]
+    #[error("ssl error: {0:?}")]
     NativeTls(#[from] native_tls::Error),
     #[cfg(feature = "openssl")]
-    #[error("ssl error")]
+    #[error("ssl error: {0:?}")]
     OpenSsl(#[from] openssl::error::ErrorStack),
     #[error("could not connect: {}", addr)]
     CouldNotConnect { addr: String, source: Box<Error> },
--- src/image.rs
+++ src/image.rs
@@ -40,21 +40,29 @@ pub struct Image {
     pub Id: String,
     pub RepoTags: Vec<String>,
     pub RepoDigests: Vec<String>,
-    pub Parent: String,
-    pub Comment: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub Parent: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub Comment: Option<String>,
     #[serde(with = "format::datetime_rfc3339")]
-    /// https://github.com/moby/moby/blob/611b23c1a0e9a9f440165a331964923fd1116256/daemon/images/image_inspect.go#L72
+    // https://github.com/moby/moby/blob/611b23c1a0e9a9f440165a331964923fd1116256/daemon/images/image_inspect.go#L72
     pub Created: DateTime<FixedOffset>,
-    pub Container: String,
-    pub ContainerConfig: Config,
-    pub DockerVersion: String,
-    pub Author: String,
+    #[deprecated]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub Container: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub DockerVersion: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub Author: Option<String>,
     pub Config: Config,
     pub Architecture: String,
     pub Os: String,
     pub Size: i64,
-    pub VirtualSize: i64,
-    pub GraphDriver: GraphDriver,
+    #[deprecated]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub VirtualSize: Option<i64>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub GraphDriver: Option<GraphDriver>,
     pub RootFS: RootFS,
 }
 
